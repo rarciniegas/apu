3af2e76a4416a3244c023588e12d4bf49aa4555d
rarciniegas <rrarciniegas@hotmail.com>
2025-09-17 09:12:02 -0400

    add Bootstrap styling

--- apu/settings.py
+++ apu/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd Party Apps
+    "crispy_forms",
+    "crispy_bootstrap5",
+    # Local Apps
     "accounts",
+    "pages",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +129,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.CustomUser"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
